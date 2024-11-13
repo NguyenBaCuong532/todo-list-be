f74a46a4aea128aae9320a63c0644f79bc47ee50
NguyenBaCuong532 <cuongmja532@gmail.com>
2024-11-14 00:14:08 +0700

    ok ok

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { User } from './type-orm/entities/user.entity';
-import { Todo } from './type-orm/entities/Todo.entity';
+import { Todo } from './type-orm/entities/todo.entity';
 @Module({
   imports: [
     // TypeOrmModule.forRoot(
