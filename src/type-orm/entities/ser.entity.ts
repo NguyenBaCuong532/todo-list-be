7ae85ccbaf7b4056cb1dd39d51daea90dba7f4ba
NguyenBaCuong532 <cuongmja532@gmail.com>
2024-11-14 09:47:45 +0700

    .

--- src/type-orm/entities/ser.entity.ts
+++ src/type-orm/entities/ser.entity.ts
@@ -1,5 +1,5 @@
 import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
-import { Todo } from "./Todo2.entity";
+import { Todo } from "./odo.entity";
 
 @Entity({name:'users'})
 export class User{
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './controllers/users/users.controller';
 import { UsersService } from './services/users/users.service';
-import { User } from 'src/type-orm/entities/User2.entity';
-import { Todo } from 'src/type-orm/entities/Todo2.entity';
+import { User } from 'src/type-orm/entities/ser.entity';
+import { Todo } from 'src/type-orm/entities/odo.entity';
 
 @Module({
     imports:[TypeOrmModule.forFeature([User,Todo])],
